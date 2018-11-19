8fb134e838a75c45c29f06c48297321cc70dccaf
Rick Lan <ricklan@gmail.com>
2018-11-19 10:41:38 +1000

    more revert

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -244,8 +244,8 @@ class CarController(object):
     if (frame % 100 == 0 or send_ui) and ECU.CAM in self.fake_ecus:
       can_sends.append(create_ui_command(self.packer, steer, sound1, sound2))
 
-    # if frame % 100 == 0 and ECU.DSU in self.fake_ecus:
-    #   can_sends.append(create_fcw_command(self.packer, fcw))
+    if frame % 100 == 0 and ECU.DSU in self.fake_ecus:
+      can_sends.append(create_fcw_command(self.packer, fcw))
 
     #*** static msgs ***
 
