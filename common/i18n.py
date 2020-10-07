9baf7bfdc7c9e8a5acd5cb7d4d3475af84c205b5
Rick Lan <ricklan@gmail.com>
2020-10-07 16:17:36 +1000

    fix get prop

--- common/i18n.py
+++ common/i18n.py
@@ -1,10 +1,10 @@
 import gettext
-from common import android
+from common.hardware_android import getprop
 
 is_android = android.ANDROID
 locale_dir = '/data/openpilot/selfdrive/assets/locales'
 supported_language = ['en-US', 'zh-TW', 'zh-CN', 'ja-JP', 'ko-KR']
-locale = android.getprop("persist.sys.locale") if is_android else 'en-US'
+locale = getprop("persist.sys.locale") if is_android else 'en-US'
 
 def events():
   i18n = gettext.translation('events', localedir=locale_dir, fallback=True, languages=[locale])
